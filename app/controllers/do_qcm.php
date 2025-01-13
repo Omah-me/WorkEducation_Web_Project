9f5e2c548a4de1918f35a8e83133a6ee05401cb9
om418031 <Omar-Ahcene_Medaouri@etu.u-bourgogne.fr>
2025-01-13 13:41:39 +0100

    Add Files from Local - 13/01/2025

---
+++ app/controllers/do_qcm.php
@@ -0,0 +1,21 @@
+<?php
+
+class Do_qcm extends Controller
+{
+    function index()
+    {   $qcm = array() ; 
+        $data['page_title'] = "Faire Les QCM";  
+        $cour="informatique" ; 
+        $data['cour'] = $cour ; 
+        $modelQCM  =  $this->loadModel("modelQCM") ; 
+        $QCM  =$modelQCM->recupererQCM($cour , "/Applications/XAMPP/xamppfiles/htdocs/WorkEdu_Web_project/app/models/xml") ;
+        foreach($QCM as $element)
+        {   $qcm['question'] = $element['question'] ; 
+            $qcm['reponse'] = $element['reponse'] ;
+            $qcm['propositions'] = $element['propositions'] ; 
+            $data[] = $qcm  ; 
+        }
+        //show($data) ; 
+        $this->view("do_qcm", $data);
+    }
+}
